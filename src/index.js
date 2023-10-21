250a8aaf13098c1ca7a5cd5784eaaaf36fba2b31
sean <sean2337@naver.com>
2023-10-22 01:20:27 +0900

    refactor: Image렌더링 최적화

--- src/index.js
+++ src/index.js
@@ -4,19 +4,26 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './GlobalStyle';
 import Router from './routes/Router';
 import theme from './assets/theme/Theme';
+import { preloadImage } from './components/\bFunction/Image';
+import carArr from './assets/CarArr/CarArr';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// 이미지 URL 리스트
+const exImageUrls = ['CarImg/startingCar.svg', 'OtherImg/DaySelect.svg'];
+const imageUrls = carArr.map((car) => `CarImg/${car.carName}.svg`);
+// 이미지 미리 로딩
+exImageUrls.forEach((url) => preloadImage(url));
+imageUrls.forEach((url) => preloadImage(url));
 
 function setScreenHeight() {
   const vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
-  console.log('setScreenHeight function called');
 }
 
 const App = () => {
   useEffect(() => {
     setScreenHeight();
-    console.log('useEffect called');
   }, []);
 
   return (
