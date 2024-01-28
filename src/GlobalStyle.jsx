bb10817a21b850ce95ffd9fdd48dd838d145ec2f
sean2337 <sean2337@naver.com>
2024-01-28 14:30:49 +0900

    fix: FOUT 현상 해결

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -1,9 +1,5 @@
-import { createGlobalStyle } from 'styled-components';
-import reset from 'styled-reset';
-import PretendardBold from '../src/assets/fonts/Pretendard-Bold.woff';
-import PretendardSemiBold from '../src/assets/fonts/Pretendard-SemiBold.woff';
-import PretendardMedium from '../src/assets/fonts/Pretendard-Medium.woff';
-import PretendardRegular from '../src/assets/fonts/Pretendard-Regular.woff';
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
@@ -14,29 +10,7 @@ const GlobalStyle = createGlobalStyle`
       background-color: ${(props) => props.theme.colors.white};
   }
 
-  @font-face {
-    font-family: 'Pretendard-bold';
-    src: url(${PretendardBold}) format('woff');
-    font-weight: 700;
-  }
-
-  @font-face {
-    font-family: 'Pretendard-semibold';
-    src: url(${PretendardSemiBold}) format('woff');
-    font-weight: 600;
-  }
-
-  @font-face {
-    font-family: 'Pretendard-medium';
-    src: url(${PretendardMedium}) format('woff');
-    font-weight: 500;
-  }
-
-  @font-face {
-    font-family: 'Pretendard-regular';
-    src: url(${PretendardRegular}) format('woff');
-    font-weight: 400;
-  }
+  
   
   * {
     box-sizing: border-box;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-import React, { useEffect } from 'react';
-import ReactDOM from 'react-dom';
-import { ThemeProvider } from 'styled-components';
-import GlobalStyle from './GlobalStyle';
-import Router from './routes/Router';
-import theme from './assets/theme/Theme';
-import { preloadImage } from './components/\bFunction/Image';
-import carArr from './assets/CarArr/CarArr';
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+import "./assets/fonts/font.css";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./GlobalStyle";
+import Router from "./routes/Router";
+import theme from "./assets/theme/Theme";
+import { preloadImage } from "./components/\bFunction/Image";
+import carArr from "./assets/CarArr/CarArr";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // 이미지 URL 리스트
 const exImageUrls = [
-  'CarImg/startingCar.svg',
-  'OtherImg/DaySelect.svg',
-  'OtherImg/Loading.gif',
-  '/SafeWeb/map_Loading.gif',
+  "CarImg/startingCar.svg",
+  "OtherImg/DaySelect.svg",
+  "OtherImg/Loading.gif",
+  "/SafeWeb/map_Loading.gif",
 ];
 const imageUrls = carArr.map((car) => `CarImg/${car.carName}.svg`);
 // 이미지 미리 로딩
@@ -23,7 +24,7 @@ imageUrls.forEach((url) => preloadImage(url));
 
 function setScreenHeight() {
   const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
 }
 
 const App = () => {
